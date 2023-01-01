cd61b3fd3348a317dcfc67efe64eab49861b233b
Denis-Bez <v417459@yandex.ru>
2023-01-01 20:23:08 +0500

    Do asynchronous ping

--- utilities/ping_check.py
+++ utilities/ping_check.py
@@ -1,4 +1,5 @@
 from pythonping import ping
+import asyncio
 
 # TO DO: How doing this asynchronous
 # Checking ping server. Example ip: "85.193.93.171", "worldcadabra.com"
@@ -16,5 +17,25 @@ def check_ping(ip):
         return(["Server offline", ping_time])
 
 
+async def async_check_ping(ip):
+    try:
+        ping_time = ping(str(ip)).rtt_avg_ms
+    except Exception as e:
+        print(e)
+        return(["Input adress error", False])
+
+    if int(ping_time) < 2000:
+        print(ping_time)
+        return(["Server online", ping_time])
+    else:
+        print(ping_time)
+        return(["Server offline", ping_time])
+
+async def async_run():
+    await async_check_ping('85.193.93.171')
+    await async_check_ping('46.147.41.173')
+
 if __name__ == '__main__':
-    print(check_ping('85.193.93.171'))
+    # print(check_ping('85.193.93.171'))
+    asyncio.run(async_run())
+    
