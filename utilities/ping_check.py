01d219f3ded3c528d0d5e8ae7390b29295e03d20
Denis-Bez <v417459@yandex.ru>
2023-01-06 15:56:33 +0500

    Rewiew async ping function

--- utilities/ping_check.py
+++ utilities/ping_check.py
@@ -33,29 +33,34 @@ def check_ping(ip):
         return(["Server offline", ping_time])
 
 
-async def async_check_ping(ip):
-    ping_time = ping(str(ip)).rtt_avg_ms
-    print(ping_time)
-
-
 async def ping_aio_check(host):
     try:
-        delay = await aioping.ping(host, timeout=2) * 1000
-        print("Ping response in %.0f ms" % delay)
+        delay = await aioping.ping(host, timeout=5) * 1000
+        return(["Server online", delay])
+        # print("Ping response in %.0f ms" % delay)
 
     except TimeoutError:
-        print("Time out")
+        return(["Server offline", 5000])
+    
+    except Exception as e:
+        print(e)
+        return(["Input adress error", False])
 
 
 if __name__ == '__main__':
-    hosts = ['46.147.41.173', '85.193.93.171', '85.193.93.171', '85.193.93.171', '85.193.93.171', '46.147.41.173', '46.147.41.173', '46.147.41.173', '46.147.41.173']
-    # run()
-    start_time = time.time()
-    for host in hosts:
-        # check_ping(host)
-        asyncio.run(ping_aio_check(host))
-    end_time = time.time()
-    print(end_time - start_time)
+    
+    # print(asyncio.run(ping_aio_check('worldcadabra.com')))
+    print(asyncio.run(ping_aio_check('https://habr.com/')))
+    # print(asyncio.run(ping_aio_check('worldcadabra.com')))
+    # hosts = ['46.147.41.173', '85.193.93.171', '85.193.93.171', '85.193.93.171', '85.193.93.171', '46.147.41.173', '46.147.41.173', '46.147.41.173', '46.147.41.173']
+    # # run()
+    # start_time = time.time()
+    # for host in hosts:
+    #     # check_ping(host)
+    #     asyncio.run(ping_aio_check(host))
+    # end_time = time.time()
+    # print(end_time - start_time)
+
 
     
     
