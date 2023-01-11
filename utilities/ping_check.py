6765069484057bbe27a842c63ab0ec1871397333
Denis-Bez <v417459@yandex.ru>
2023-01-11 17:32:20 +0500

    Async ping utilite review

--- utilities/ping_check.py
+++ utilities/ping_check.py
@@ -6,10 +6,10 @@ import aioping
 
 # TO DO: How doing this asynchronous
 # Checking async ping server. Example ip: "85.193.93.171", "worldcadabra.com"
-async def ping_aio_check(host):
+async def check_ping(host):
     try:
         delay = await aioping.ping(host, timeout=5) * 1000
-        return(["Server online", delay])
+        return(["Server online", int(delay)])
         # print("Ping response in %.0f ms" % delay)
 
     except TimeoutError:
@@ -21,18 +21,9 @@ async def ping_aio_check(host):
 
 
 if __name__ == '__main__':
-    
-    # print(asyncio.run(ping_aio_check('worldcadabra.com')))
-    print(asyncio.run(ping_aio_check('https://habr.com/')))
-    # print(asyncio.run(ping_aio_check('worldcadabra.com')))
-    # hosts = ['46.147.41.173', '85.193.93.171', '85.193.93.171', '85.193.93.171', '85.193.93.171', '46.147.41.173', '46.147.41.173', '46.147.41.173', '46.147.41.173']
-    # # run()
-    # start_time = time.time()
-    # for host in hosts:
-    #     # check_ping(host)
-    #     asyncio.run(ping_aio_check(host))
-    # end_time = time.time()
-    # print(end_time - start_time)
+
+    print(asyncio.run(check_ping('habr.com')))
+
 
 
     
